8c174fa41c28ad2580e83c43f6965ba7fa3abc33
Andydo21 <120573918+Andydo21@users.noreply.github.com>
2024-11-21 13:12:39 +0700

    Add files via upload

---
+++ LoginForm.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package userauthapp;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Windows 10
+ */
+class LoginForm extends JFrame implements ActionListener {
+    JTextField usernameField;
+    JPasswordField passwordField;
+    JButton loginButton;
+
+    public LoginForm() {
+        setTitle("Login");
+        setSize(300, 150);
+        setLayout(new GridLayout(3, 2));
+
+
+        usernameField = new JTextField();
+        passwordField = new JPasswordField();
+        loginButton = new JButton("Login");
+
+        add(new JLabel("Username:"));
+        add(usernameField);
+        add(new JLabel("Password:"));
+        add(passwordField);
+        add(loginButton);
+
+        loginButton.addActionListener(this);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String username = usernameField.getText();
+        String password = new String(passwordField.getPassword());
+
+        if (User(username, password)) {
+            JOptionPane.showMessageDialog(this, "Success login");
+        } else {
+            JOptionPane.showMessageDialog(this, "User not exist");
+        }
+    }
+
+    private boolean User(String username, String password) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("users.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userDetails = line.split(",");
+                if (userDetails[0].equals(username) && userDetails[1].equals(password)) {
+                    return true;
+                }
+            }
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this, "Error during login.");
+        }
+        return false;
+    }
+}
---
+++ UserAuthApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package userauthapp;
+
+/**
+ *
+ * @author Windows 10
+ */
+
+
+public class UserAuthApp {
+    public static void main(String[] args) {
+        new RegisterForm();
+    }
+}
+
